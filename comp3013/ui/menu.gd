4e2f7be3188788472ddf2049698c0dbcdb51a56e
Michael Hooper <mikeyhooper360@gmail.com>
2026-02-15 11:42:37 +0000

    Added michael test scene

--- comp3013/ui/menu.gd
+++ comp3013/ui/menu.gd
@@ -10,7 +10,8 @@ func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/oliver_test.tscn")
 
 func _on_button_3_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/levels/michael_test.tscn")
 
 func _on_button_4_pressed() -> void:
 	pass # Replace with function body.
