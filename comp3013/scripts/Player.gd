d9bb759f57981ea8713ec2e057a1e680887c8fbc
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2026-03-20 20:52:43 +0000

    Added outline on buttons and skill points to skill tree

--- comp3013/scripts/Player.gd
+++ comp3013/scripts/Player.gd
@@ -2,6 +2,11 @@ extends Entity
 
 class_name Player
 
+@onready var skillPoints : int = 5
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _init() -> void:
+	skillPoints = 5
