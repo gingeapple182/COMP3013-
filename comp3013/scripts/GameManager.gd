91b375955af94409db35f3bd0dcf7e4f72634a9b
Theo Butler <146274190+Thefudgeman@users.noreply.github.com>
2026-03-23 11:05:16 +0000

    Update GameManager.gd

--- comp3013/scripts/GameManager.gd
+++ comp3013/scripts/GameManager.gd
@@ -2,13 +2,13 @@ extends Node
 
 @onready var skillPoints : int
 
-@onready var Entropy : bool 
-@onready var Reputation : bool  
-@onready var Slayer : bool 
-@onready var Courier : bool
-@onready var Banker : bool
-@onready var Collection : bool
-@onready var Informant : bool
+@onready var Entropy : int
+@onready var Reputation : int  
+@onready var Slayer : int
+@onready var Courier : int
+@onready var Banker : int
+@onready var Collection : int
+@onready var Informant : int
 @onready var className : String
 @onready var playerLevel : int
 
