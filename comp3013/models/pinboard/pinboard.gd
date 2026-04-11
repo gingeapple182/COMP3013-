b8c98c4bb79be70fb986889edb473d56e519af83
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2026-04-11 18:13:04 +0100

    correct number of quests show on pinboard but only displays array index for info

--- comp3013/models/pinboard/pinboard.gd
+++ comp3013/models/pinboard/pinboard.gd
@@ -1,10 +1,16 @@
 extends StaticBody3D
 @onready var canvas_layer: CanvasLayer = $model/CanvasLayer
-
+@onready var pinboardItem = preload("res://models/pinboard/pinboardItem.tscn")
+@onready var grid_container: GridContainer = $model/CanvasLayer/TextureRect/GridContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	canvas_layer.hide()
+	for i in range(GameManager.npcQuests.size()):
+		if GameManager.npcQuests[i] == 1:
+			var Item = pinboardItem.instantiate()
+			Item.get_child(0).get_child(1).text = str(i)
+			grid_container.add_child(Item)
 	pass # Replace with function body.
 
 
@@ -14,6 +20,7 @@ func _process(delta: float) -> void:
 
 func interact() -> void:
 	print("Interact")
+	
 	canvas_layer.show()
 	GameManager.uiOpen = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- comp3013/scripts/GameManager.gd
+++ comp3013/scripts/GameManager.gd
@@ -12,9 +12,23 @@ extends Node
 @onready var className : String
 @onready var playerLevel : int
 @onready var uiOpen : bool = false
+@onready var npcQuests = [0,0,0,0,0,0,0,0]
+@onready var acceptedQuests = [0,0,0,0,0,0,0,0]
 
 
 
 func _ready() -> void:
 	skillPoints = 1
 	playerLevel = 1
+	newDay()
+
+func newDay() -> void: #use this function to change what is needed when a new day starts
+	var rand = RandomNumberGenerator.new()
+	var numOfQuests = rand.randi_range(2,6)
+	var questRange = range(0,7)
+	questRange.shuffle()
+	questRange = questRange.slice(0,numOfQuests)
+	for i in range(numOfQuests):
+		npcQuests[questRange[i]] = 1
+	print(npcQuests)
+		
